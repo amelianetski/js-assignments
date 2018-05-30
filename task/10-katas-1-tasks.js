487be40f18a85d8a5d8f7f05b434b67b4dbd4766
omelenetskiy <amelianetski@gmail.com>
2018-05-30 14:49:26 +0300

    katas

--- task/10-katas-1-tasks.js
+++ task/10-katas-1-tasks.js
@@ -83,8 +83,20 @@ export function createCompassPoints(sides = ['N', 'E', 'S', 'W']) {
  *   'nothing to do' => 'nothing to do'
  */
 export function* expandBraces(str) {
-  /* implement your code here */
-  throw new Error('Not implemented');
+  const input = [str],
+    exist = [];
+  while (input.length > 0) {
+    str = input.shift();
+    let match = str.match(/\{([^{}]+)\}/);
+    if (match) {
+      for (let value of match[1].split(',')) {
+        input.push(str.replace(match[0], value));
+      }
+    } else if (exist.indexOf(str) < 0) {
+      exist.push(str);
+      yield str;
+    }
+  }
 }
 
 /**
@@ -116,8 +128,23 @@ export function* expandBraces(str) {
  *
  */
 export function getZigZagMatrix(n) {
-  /* implement your code here */
-  throw new Error('Not implemented');
+  const mtx = [];
+  for (let i = 0; i < n; i++) mtx[i] = [];
+  let i = 1,
+    j = 1;
+  for (let e = 0; e < n * n; e++) {
+    mtx[i - 1][j - 1] = e;
+    if ((i + j) % 2 === 0) {
+      if (j < n) j++;
+      else i += 2;
+      if (i > 1) i--;
+    } else {
+      if (i < n) i++;
+      else j += 2;
+      if (j > 1) j--;
+    }
+  }
+  return mtx;
 }
 
 /**
@@ -142,8 +169,52 @@ export function getZigZagMatrix(n) {
  *
  */
 export function canDominoesMakeRow(dominoes) {
-  /* implement your code here */
-  throw new Error('Not implemented');
+  let res = false,
+    cnt = Array.from({ length: 7 }, () => 0);
+  dominoes = dominoes.filter((v, i) => {
+    if (v[0] !== v[1]) {
+      cnt[v[0]]++;
+      cnt[v[1]]++;
+      return true;
+    } else {
+      if (
+        !dominoes.some(
+          (v1, i1) => i !== i1 && (v[0] === v1[0] || v[0] === v1[1])
+        )
+      ) {
+        res = true;
+      }
+      return false;
+    }
+  });
+  if (res) return false;
+  if (dominoes.length === 1) return true;
+  res = 0;
+  for (let i = 0; i < 7; i++) if (cnt[i] % 2) res++;
+  if (res > 2) return false;
+  function* combine(domino1, domino2) {
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        if (domino1[i] === domino2[j]) yield [domino1[1 - i], domino2[1 - j]];
+      }
+    }
+  }
+  const exclude = Array.from({ length: dominoes.length }, (v, i) => i);
+  let excludeCount = 1;
+  function rec(domino) {
+    if (excludeCount === dominoes.length) return true;
+    for (let i = 1; i < dominoes.length; i++) {
+      if (exclude[i]) {
+        exclude[i] = false;
+        excludeCount++;
+        for (let v of combine(domino, dominoes[i])) if (rec(v)) return true;
+        exclude[i] = true;
+        excludeCount--;
+      }
+    }
+    if (excludeCount === 1) return false;
+  }
+  return rec(dominoes[0]);
 }
 
 /**
@@ -168,6 +239,15 @@ export function canDominoesMakeRow(dominoes) {
  * [ 1, 2, 4, 5]          => '1,2,4,5'
  */
 export function extractRanges(nums) {
-  /* implement your code here */
-  throw new Error('Not implemented');
+  let arr = [[nums[0]]];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - 1 === arr[arr.length - 1][arr[arr.length - 1].length - 1]) {
+      arr[arr.length - 1].push(nums[i]);
+    } else arr.push([nums[i]]);
+  }
+  for (let i in arr) {
+    if (arr[i].length > 2) arr[i] = arr[i][0] + '-' + arr[i][arr[i].length - 1];
+    else arr[i] = arr[i].join(',');
+  }
+  return arr.join(',');
 }
--- task/12-katas-3-tasks.js
+++ task/12-katas-3-tasks.js
@@ -1,4 +1,3 @@
-
 /**
  * Returns true if word occurrs in the specified word snaking puzzle.
  * Each words can be constructed using "snake" path inside a grid with top, left,
@@ -28,10 +27,34 @@
  *   'NULL'      => false
  */
 export function findStringInSnakingPuzzle(puzzle, searchStr) {
-  /* implement your code here */
-  throw new Error('Not implemented');
-}
+  function rec(exclude) {
+    if (searchStr.length === exclude.length) return true;
+    let pos = exclude[exclude.length - 1];
+    for (let val of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
+      if (
+        pos[0] + val[0] >= 0 &&
+        pos[0] + val[0] < puzzle.length &&
+        pos[1] + val[1] >= 0 &&
+        pos[1] + val[1] < puzzle[pos[0] + val[0]].length &&
+        !exclude.some(
+          v => v[0] === pos[0] + val[0] && v[1] === pos[1] + val[1]
+        ) &&
+        puzzle[pos[0] + val[0]][pos[1] + val[1]] === searchStr[exclude.length]
+      ) {
+        if (rec(exclude.concat([[pos[0] + val[0], pos[1] + val[1]]]))) {
+          return true;
+        }
+      }
+    }
+  }
 
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[i].length; j++) {
+      if (puzzle[i][j] === searchStr[0] && rec([[i, j]])) return true;
+    }
+  }
+  return false;
+}
 
 /**
  * Returns all permutations of the specified string.
@@ -47,10 +70,17 @@ export function findStringInSnakingPuzzle(puzzle, searchStr) {
  *    'abc' => 'abc','acb','bac','bca','cab','cba'
  */
 export function* getPermutations(chars) {
-  /* implement your code here */
-  throw new Error('Not implemented');
-}
+  function* rec(str) {
+    if (str.length === chars.length) yield str;
+    else {
+      for (let i = 0; i < chars.length; i++) {
+        if (str.indexOf(chars[i]) < 0) yield* rec(str + chars[i]);
+      }
+    }
+  }
 
+  yield* rec('');
+}
 
 /**
  * Returns the most profit from stock quotes.
@@ -70,10 +100,17 @@ export function* getPermutations(chars) {
  *    [ 1, 6, 5, 10, 8, 7 ] => 18  (buy at 1,6,5 and sell all at 10)
  */
 export function getMostProfitFromStockQuotes(quotes) {
-  /* implement your code here */
-  throw new Error('Not implemented');
+  let result = 0,
+    quotesSorted = quotes.slice(0).sort((a, b) => b - a);
+  while (quotes.length) {
+    while (quotes.indexOf(quotesSorted[0]) < 0) quotesSorted.shift();
+    let inx = quotes.indexOf(quotesSorted[0]);
+    result += quotesSorted[0] * inx;
+    for (let i = 0; i < inx; i++) result -= quotes[i];
+    quotes.splice(0, inx + 1);
+  }
+  return result;
 }
-
 
 /**
  * Class representing the url shorting helper.
@@ -90,18 +127,68 @@ export function getMostProfitFromStockQuotes(quotes) {
  *
  */
 export function UrlShortener() {
-  this.urlAllowedChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-                          'abcdefghijklmnopqrstuvwxyz' +
-                          "0123456789-_.~!*'();:@&=+$,/?#[]";
+  this.urlAllowedChars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+    'abcdefghijklmnopqrstuvwxyz' +
+    "0123456789-_.~!*'();:@&=+$,/?#[]";
 }
 
 UrlShortener.prototype = {
+  encode: function(url) {
+    let code = url.slice(8);
+    code = code
+      .split('')
+      .map(val => {
+        let cur = '' + (val.charCodeAt(0) - 27);
 
-  encode(url) {
-    throw new Error('Not implemented');
+        if (cur.length === 1) return '0' + cur;
+        return cur;
+      })
+      .join('');
+
+    let codeSequence = [];
+
+    for (let i = 3; i <= code.length; i += 3) {
+      codeSequence.push(code.slice(i - 3, i));
+
+      if (code.length - i < 3) {
+        let repeat = code.length - i > 1 ? 1 : 2;
+        codeSequence.push(code.slice(i) + '0'.repeat(repeat));
+        break;
+      }
+    }
+
+    code = codeSequence.map(val => String.fromCharCode(val)).join('');
+
+    return code;
   },
 
-  decode(code) {
-    throw new Error('Not implemented');
+  decode: function(code) {
+    let url = code
+      .split('')
+      .map(val => {
+        let cur = '' + val.charCodeAt(0);
+        if (cur.length === 2) return '0' + cur;
+        else if (cur.length === 1) return '00' + cur;
+        return cur;
+      })
+      .join('');
+
+    let urlSequence = [];
+
+    for (let i = 2; i <= url.length; i += 2) {
+      urlSequence.push(url.slice(i - 2, i));
+    }
+
+    url = urlSequence
+      .map(val => {
+        if (+val !== 0) return String.fromCharCode(+val + 27);
+        else if (val[0] !== '0' && val[1] === '0') {
+          return String.fromCharCode(+val.charAt(0) + 27);
+        }
+      })
+      .join('');
+
+    return 'https://' + url;
   }
 };
